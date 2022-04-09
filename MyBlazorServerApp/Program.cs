8ee0c58b06269046d2f23d634f5c2b83a8e2f587
SquishyBootz <81831693+SquishyBootz@users.noreply.github.com>
2022-04-09 15:37:44 -0400

    1. Added Razor Components     Military History     Software Development    Management 2. Hooked up new razor comps to nav 3. Built out Military component 4. Built out Section Hub navigator (about)

--- MyBlazorServerApp/Program.cs
+++ MyBlazorServerApp/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
 using MyBlazorServerApp.Controllers;
 using MyBlazorServerApp.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
