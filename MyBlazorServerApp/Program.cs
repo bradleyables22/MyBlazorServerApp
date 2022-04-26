302b776f63c88c82cfabeaef0a0bdd24b6108bff
SquishyBootz <81831693+SquishyBootz@users.noreply.github.com>
2022-04-26 11:17:11 -0400

    Extended HSTS, preload to true, include subdomains to true

--- MyBlazorServerApp/Program.cs
+++ MyBlazorServerApp/Program.cs
@@ -11,7 +11,15 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddDbContext<PortfolioDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-//scoped services&controllers 
+//Extend HSTS
+builder.Services.AddHsts(options =>
+{
+    options.Preload = true;
+    options.IncludeSubDomains = true;
+    options.MaxAge = TimeSpan.FromDays(360);
+});
+
+//scoped services/controllers 
 builder.Services.AddScoped<GuestEntriesController>();
 builder.Services.AddScoped<StarwarsController>();
 builder.Services.AddScoped<UserController>();
@@ -23,6 +31,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    //HSTS extended to one year
     app.UseHsts();
 }
 
